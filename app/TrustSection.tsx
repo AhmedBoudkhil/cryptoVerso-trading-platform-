ceea311cf8c978a37a1075a22837ecad5a8f6558
AhmedBoudkhil <ahmedbdlm362@gmail.com>
2025-10-21 18:06:02 +0200

    we add icons and styles to trustsection and enhanced svg

--- app/TrustSection.tsx
+++ app/TrustSection.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./TrustSection.css";
+<<<<<<< HEAD
 import safe from "./assets/icons/safe-3.svg";
 import suport from "./assets/icons/support-5.svg";
 import world from "./assets/icons/worldwide-3.svg";
 import economy from "./assets/icons/economyGlobal-3.svg";
+=======
+
+
+import safe from "../assets/safe-3.svg";
+import worldwide  from "../assets/worldwide-3.svg"
+import global from "../assets/economyGlobal-3.svg"
+import support from "../assets/support-5.svg"
+
+
+>>>>>>> 315f124 (we add icnons and styles to trustsection adn enhanced svg)
 
 const TrustSection : React.FC = () => {
     return(
@@ -19,22 +30,38 @@ const TrustSection : React.FC = () => {
             
             <div className="cards">
                 <div className="card">
+<<<<<<< HEAD
                     <img src={world}alt="" />
+=======
+                    <img src="assets/worldwide-3.svg" alt="" />
+>>>>>>> 315f124 (we add icnons and styles to trustsection adn enhanced svg)
                     <h3>The largest public crypto company</h3>
                     <p>We operate with financial transparency.</p>
                 </div>
                 <div className="card">
+<<<<<<< HEAD
                     <img src={safe}alt="" />
+=======
+                    <img src="/assets/safe-3.svg" alt="" />
+>>>>>>> 315f124 (we add icnons and styles to trustsection adn enhanced svg)
                     <h3>You can always contact our support team for quick solutions to common problems.</h3>
                     <p>Our risk management measures are designed to protect your assets.</p>
                 </div>
                 <div className="card">
+<<<<<<< HEAD
                     <img src={suport} alt="" />
+=======
+                    <img src="/assets/support-5.svg" alt="" />
+>>>>>>> 315f124 (we add icnons and styles to trustsection adn enhanced svg)
                     <h3>Get the help you need, when you need it</h3>
                     <p>You can always contact our support team for quick solutions to common problems.</p>
                 </div>
                 <div className="card">
+<<<<<<< HEAD
                     <img src={economy} alt="" />
+=======
+                    <img src="/assets/economyGlobal-3.svg" alt="" />
+>>>>>>> 315f124 (we add icnons and styles to trustsection adn enhanced svg)
                     <h3>Industry best practices</h3>
                     <p>Coinbase supports a variety of the most popular digital currencies.</p>
                 </div>
